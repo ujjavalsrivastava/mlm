5506cf144d31c06274c0e96cbbd2c52a62253f8a
codio-lab <akramraza962@gmail.com>
2024-08-11 15:57:04 +0530

    feature level percentage

--- backend/routes/product-route.js
+++ backend/routes/product-route.js
@@ -8,14 +8,15 @@ const {
   createRazorpayOrder,
 } = require("../controllers/productController");
 const { adminAuth, auth } = require("../middleware/auth");
+const { tryCatch } = require("../utils/helper");
 
 const router = new Router();
 
-router.get("/product/order", auth, getProductOrder);
-router.post("/product/order", auth, createProductOrder);
-router.post("/create/order", auth, createRazorpayOrder);
-router.post("/product", adminAuth, createProduct);
-router.get("/product/:id", auth, getProduct);
-router.put("/product/:id", adminAuth, updateProduct);
+router.get("/product/order", tryCatch(auth), tryCatch(getProductOrder));
+router.post("/product/order", tryCatch(auth), tryCatch(createProductOrder));
+router.post("/create/order", tryCatch(auth), tryCatch(createRazorpayOrder));
+router.post("/product", tryCatch(adminAuth), tryCatch(createProduct));
+router.get("/product/:id", tryCatch(auth), tryCatch(getProduct));
+router.put("/product/:id", tryCatch(adminAuth), tryCatch(updateProduct));
 
 module.exports = router;
