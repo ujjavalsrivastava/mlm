9a173d80315c9642007cc832b084b3c7a52937bc
codio-lab <akramraza962@gmail.com>
2024-08-15 13:16:18 +0530

    add logger and history

--- backend/controllers/purchaseController.js
+++ backend/controllers/purchaseController.js
@@ -1,6 +1,7 @@
 const UserPurchase = require("../models/purchase-history-model");
 const PercentDistribution = require("../models/percentage-distribution-model");
 const { handlePromiseError, getUserId } = require("../utils/helper");
+const { oneDayAgo, oneMonthAgo, oneWeekAgo } = require("../utils/dateTime");
 
 const handleProductPurchase = async (req, res) => {};
 const getUserProductsAndBalance = async (req, res) => {
@@ -16,13 +17,32 @@ const getUserProductsAndBalance = async (req, res) => {
 
 const getUserPercentDistribution = async (req, res) => {
   const userId = getUserId(req);
+
   const [error, percentDistribution] = await handlePromiseError(
     PercentDistribution.findOne({ userId })
   );
+  let oneDayEarning = 0;
+  let oneWeekEarning = 0;
+  let oneMonthEarning = 0;
+  let overallEarning = 0;
+  if (percentDistribution?.purchaseHistory?.length) {
+    percentDistribution.purchaseHistory.forEach((h) => {
+      if (h.createdAt > oneDayAgo()) {
+        oneDayEarning += h.amount;
+      }
+      if (h.createdAt > oneWeekAgo()) {
+        oneWeekEarning += h.amount;
+      }
+      if (h.createdAt > oneMonthAgo()) {
+        oneMonthEarning += h.amount;
+      }
+      overallEarning += h.amount;
+    });
+  }
   if (error) {
     return res.status(400).json({ error });
   }
-  res.json(percentDistribution);
+  res.json({ oneDayEarning, oneWeekEarning, oneMonthEarning, overallEarning });
 };
 const getUserAccountAndPurcheseHistory = async (req, res) => {
   const userId = getUserId(req);
