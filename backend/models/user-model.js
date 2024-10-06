f8a23794423abd112c953cb60e15446d570c165f
ujjvalsrivastava <ujjval000@gmail.com>
2024-10-07 00:09:38 +0530

    valist

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -39,10 +39,6 @@ const userSchema = new Schema(
       type: String,
       require: true,
       minLength: 6,
-      validate(value) {
-        if (value.toLowerCase().includes("password"))
-          throw new Error("Password can not contain password");
-      },
     },
   },
   {
