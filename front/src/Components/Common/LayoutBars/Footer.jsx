be1508d2464f71ac955a67d8b2271213b273bbcb
ujjvalsrivastava <ujjval000@gmail.com>
2024-10-27 16:07:09 +0530

    dfad

--- front/src/Components/Common/LayoutBars/Footer.jsx
+++ front/src/Components/Common/LayoutBars/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <>
       <footer className="main-footer">
         <div className="pull-right hidden-xs">Version 1.2</div>
-        Copyright © 2024 Yourdomian. All rights reserved.
+        Copyright © 2024 digitalduniyaa.in. All rights reserved.
       </footer>
     </>
   );
