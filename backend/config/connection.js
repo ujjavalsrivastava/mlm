0e8600b042ec24492ed11ca9b917003c2a5eca2f
ujjvalsrivastava <ujjval000@gmail.com>
2024-12-02 23:26:42 +0530

    upsdfasd

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-    process.env.MONGODB_CONNECTION_URL
+    `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@mlmclusters.7madj9i.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority&appName=${process.env.DBNAME}`
     );
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
