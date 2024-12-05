8609c6740fc2bd1d6f7474f816914291f86e995c
ujjvalsrivastava <ujjval000@gmail.com>
2024-12-05 20:43:41 +0530

    upyy

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@mlmclusters.7madj9i.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority&appName=${process.env.DBNAME}`
-    );
+   // `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@mlmclusters.7madj9i.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority&appName=${process.env.DBNAME}`
+   `mongodb+srv://digitalduniya2407:o99puertAeGh3Zk6@cluster0.d2mub.mongodb.net/digitduniyadb?retryWrites=true&w=majority&appName=digitduniyadb`  
+  );
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
